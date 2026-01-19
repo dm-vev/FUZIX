756465d53f0fb0e696226038206f82be3696c14a
dm-vev <carmakdoom@gmail.com>
2026-01-19 07:52:35 +0000

    pico: add pollcfg/power/kbdlight ioctl ABI

--- Kernel/platform/platform-rpipico/pico_ioctl.h
+++ Kernel/platform/platform-rpipico/pico_ioctl.h
@@ -15,6 +15,19 @@
 /* Get PicoCalc I2C diagnostics */
 #define PICOIOC_GET_I2C_STATS 0x0004
 
+/* Get/Set PicoCalc I2C poll config */
+#define PICOIOC_GET_POLL_CONFIG 0x0005
+#define PICOIOC_SET_POLL_CONFIG 0x0006
+
+/* Set PicoCalc keyboard backlight (percent 0-100) */
+#define PICOIOC_SET_KBD_BACKLIGHT 0x0007
+
+/* Power off PicoCalc (seconds >=6 recommended; 0 => default) */
+#define PICOIOC_POWEROFF 0x0008
+
+/* Reset PicoCalc keyboard/PMU MCU (delay seconds; 0 => default 1s) */
+#define PICOIOC_RESET_KBD 0x0009
+
 enum {
 	PICOCALC_BATF_CHARGING = 0x01,
 };
@@ -38,6 +51,15 @@ struct picocalc_i2c_stats {
 	uint32_t backoff_us;
 	uint32_t last_ok_ms;
 	uint32_t last_err_ms;
+};
+
+struct picocalc_poll_config {
+	uint32_t kbd_poll_us;
+	uint32_t status_poll_us;
+	uint32_t backoff_min_us;
+	uint32_t backoff_max_us;
+	uint32_t fifo_max_per_poll;
+	uint32_t reserved[3];
 };
 
 #endif
