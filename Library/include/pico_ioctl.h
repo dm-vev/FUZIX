abca3dffe9c6f1a2eff0ef306bbc522e1d6f838f
dm-vev <carmakdoom@gmail.com>
2026-01-19 07:15:35 +0000

    lib: add pico ioctl header

---
+++ Library/include/pico_ioctl.h
@@ -0,0 +1,25 @@
+#ifndef PICO_IOCTL_H
+#define PICO_IOCTL_H
+
+#include <stdint.h>
+
+/* Reboot PI Pico into flash mode */
+#define PICOIOC_FLASH 0x0001
+
+/* Get PicoCalc status (battery/backlight/etc) */
+#define PICOIOC_GET_STATUS 0x0002
+
+enum {
+	PICOCALC_BATF_CHARGING = 0x01,
+};
+
+struct picocalc_status {
+	uint8_t battery_percent; /* 0-100, or 0xFF if unknown */
+	uint8_t battery_flags;   /* PICOCALC_BATF_* */
+	uint8_t lcd_backlight;   /* 0-255, or 0xFF if unknown */
+	uint8_t kbd_backlight;   /* 0-255, or 0xFF if unknown */
+	uint8_t fw_version;      /* raw register value, or 0xFF if unknown */
+	uint8_t reserved[3];
+};
+
+#endif
