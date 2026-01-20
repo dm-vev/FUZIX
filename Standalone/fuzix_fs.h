e6b682af2570a50f534e88b69e4e3c00b14665ff
dm-vev <carmakdoom@gmail.com>
2026-01-20 05:47:41 +0000

    Standalone: add on-disk FS v1 structs

--- Standalone/fuzix_fs.h
+++ Standalone/fuzix_fs.h
@@ -19,6 +19,18 @@
 #define IPERBLK 8
 
 /* On-disk structures for largefs (512-byte blocks only). */
+typedef struct fuzix_dinode_v1 {
+    uint16_t i_mode;
+    uint16_t i_nlink;
+    uint16_t i_uid;
+    uint16_t i_gid;
+    uint32_t i_size;
+    uint32_t i_atime;
+    uint32_t i_mtime;
+    uint32_t i_ctime;
+    uint16_t i_addr[20];
+} fuzix_dinode_v1;               /* Exactly 64 bytes long! */
+
 typedef struct fuzix_dinode_v2 {
     uint16_t i_mode;
     uint16_t i_nlink;
@@ -30,6 +42,22 @@ typedef struct fuzix_dinode_v2 {
     uint32_t i_ctime;
     uint32_t i_addr[10];
 } fuzix_dinode_v2;               /* Exactly 64 bytes long! */
+
+typedef struct fuzix_filesys_v1 {
+    uint16_t s_mounted;
+    uint16_t s_isize;
+    uint16_t s_fsize;
+    uint16_t s_nfree;
+    uint16_t s_free[50];
+    int16_t  s_ninode;
+    uint16_t s_inode[50];
+    uint8_t  s_fmod;
+    uint8_t  s_timeh;
+    uint32_t s_time;
+    uint16_t s_tfree;
+    uint16_t s_tinode;
+    uint8_t  s_shift;
+} fuzix_filesys_v1;
 
 /*
  * NOTE: Padding is explicit to make the on-disk layout stable across
